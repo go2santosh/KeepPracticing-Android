a4b332fb15de15c9cb017240ad495e045f618f99
Santosh Sharma <go2santosh@hotmail.com>
2019-05-02 09:35:33 +0530

    Added animation to splash text

--- app/src/main/java/com/go2santosh/keeppracticing/SplashActivity.kt
+++ app/src/main/java/com/go2santosh/keeppracticing/SplashActivity.kt
@@ -4,34 +4,44 @@ import kotlinx.android.synthetic.main.activity_splash.*
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 
 class SplashActivity : Activity() {
-
-    private var delayHandler: Handler? = null
-    private val SPLASH_DELAY: Long = 3000
-
-    internal val startMainActivity: Runnable = Runnable {
-        if (!isFinishing) {
-            val intent = Intent(applicationContext, MainActivity::class.java)
-            startActivity(intent)
-            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
-            finish()
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         textViewSplash.text = getString(R.string.keep_practicing)
-        delayHandler = Handler()
-        delayHandler!!.postDelayed(startMainActivity, SPLASH_DELAY)
+        animateLaunchLogo()
     }
 
-    public override fun onDestroy() {
-        if (delayHandler != null) {
-            delayHandler!!.removeCallbacks(startMainActivity)
-        }
-        super.onDestroy()
+    private fun animateLaunchLogo() {
+
+        val animation = AnimationUtils.loadAnimation(
+            applicationContext,
+            R.anim.anim_splash_text
+        )
+        animation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animation) {
+                startMainActivity()
+            }
+
+            override fun onAnimationRepeat(animation: Animation) {
+
+            }
+        })
+        textViewSplash.startAnimation(animation)
+    }
+
+    private fun startMainActivity() {
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        startActivity(intent)
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        finish()
     }
 }
