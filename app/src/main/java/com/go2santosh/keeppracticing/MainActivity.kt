d062fd273a4963e856e0b7d97e87b8bcc4839377
Santosh Sharma <santoshs2@hcl.com>
2019-05-03 22:20:38 +0530

    Added Quiz screen

--- app/src/main/java/com/go2santosh/keeppracticing/MainActivity.kt
+++ app/src/main/java/com/go2santosh/keeppracticing/MainActivity.kt
@@ -2,6 +2,7 @@ package com.go2santosh.keeppracticing
 
 import kotlinx.android.synthetic.main.activity_main.*
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 
 class MainActivity : Activity() {
@@ -10,5 +11,14 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textViewCaption.text = getString(R.string.keep_practicing)
+        buttonStart.text = getString(R.string.start)
+        buttonStart.run { setOnClickListener { startQuiz() } }
+    }
+
+    private fun startQuiz() {
+        val intent = Intent(applicationContext, QuizActivity::class.java)
+        startActivity(intent)
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        finish()
     }
 }
---
+++ app/src/main/java/com/go2santosh/keeppracticing/QuizActivity.kt
@@ -0,0 +1,12 @@
+package com.go2santosh.keeppracticing
+
+import android.app.Activity
+import android.os.Bundle
+
+class QuizActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz)
+    }
+}
